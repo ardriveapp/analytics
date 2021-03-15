8f25b69d10da37ffd16e6d1ae6c8c4eea1e3170c
Philip Mataras <vilenarios@gmail.com>
2021-03-15 13:38:11 -0400

    Correct pricing gatheting

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,8 @@ import { getArUSDPrice, getCurrentBlockHeight, getDataPrice, getLatestBlockInfo 
 import { sendMessageToGraphite, getMetrics, sendResultsToGraphite } from './common';
 import { Results, BlockInfo } from './types';
 
+// Used for scheduling the jobs
 const cron = require('node-cron');
-
-
-// ArDrive Profit Sharing Community Smart Contract
-// import Community from 'community-js';
-// const communityTxId = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ';
-
-/*
-* add number of ardrive pst holders
-•	Tips sent
-	Total size of tips
-	Total number of tips distributed
-*/
 
 async function dailyArDriveUsageAnalytics () {
     let today = new Date();
@@ -33,38 +22,40 @@ async function networkAnalytics() {
     console.log ("")
 
     let height = await getCurrentBlockHeight();
-    await sendMessageToGraphite('weave.height', +height, today)
+    await sendMessageToGraphite('arweave.blockHeight', +height, today)
     let latestBlock : BlockInfo = await getLatestBlockInfo(height)
-    await sendMessageToGraphite('weave.totalSize', latestBlock.weaveSize, today)
-    await sendMessageToGraphite('weave.difficulty', latestBlock.difficulty, today)
-    await sendMessageToGraphite('weave.lastBlockSize', latestBlock.blockSize, today)
+    await sendMessageToGraphite('arweave.weaveSize', latestBlock.weaveSize, today)
+    await sendMessageToGraphite('arweave.difficulty', latestBlock.difficulty, today)
+    await sendMessageToGraphite('arweave.lastBlockSize', latestBlock.blockSize, today)
 
     // Get price of AR in USD
     let arUSDPrice = await getArUSDPrice()
 
     // Get data prices of different data sizes in AR
     if (arUSDPrice !== 0) {
-        const priceOf1MB = await getDataPrice(1048576);
-        const priceOf5MB = await getDataPrice(1048576*5);
-        const priceOf25MB = await getDataPrice(1048576*25);
-        const priceOf100MB = await getDataPrice(1048576*100);
-        const priceOf500MB = await getDataPrice (1048576*500);
-        const priceOf1GB = await getDataPrice(1073741824);
-        await sendMessageToGraphite('price.ar.usd', arUSDPrice, today)
-        await sendMessageToGraphite('price.ar.1mb', +priceOf1MB.toFixed(5), today)
-        await sendMessageToGraphite('price.ar.5mb', +priceOf5MB.toFixed(5), today)
-        await sendMessageToGraphite('price.ar.25mb', +priceOf25MB.toFixed(5), today)
-        await sendMessageToGraphite('price.ar.100mb', +priceOf100MB.toFixed(5), today)
-        await sendMessageToGraphite('price.ar.500mb', +priceOf500MB.toFixed(5), today)
-        await sendMessageToGraphite('price.ar.1gb', +priceOf1GB.toFixed(5), today)
+
+        // Include the 15% fee
+        const priceOf1MB = await getDataPrice(1048576) * 1.15;
+        const priceOf5MB = await getDataPrice(1048576*5) * 1.15;
+        const priceOf25MB = await getDataPrice(1048576*25) * 1.15;
+        const priceOf100MB = await getDataPrice(1048576*100) * 1.15;
+        const priceOf500MB = await getDataPrice (1048576*500) * 1.15;
+        const priceOf1GB = await getDataPrice(1073741824) * 1.15;
+        await sendMessageToGraphite('arweave.price.usd', arUSDPrice, today)
+        await sendMessageToGraphite('ardrive.price.ar.1mb', +priceOf1MB.toFixed(5), today)
+        await sendMessageToGraphite('ardrive.price.ar.5mb', +priceOf5MB.toFixed(5), today)
+        await sendMessageToGraphite('ardrive.price.ar.25mb', +priceOf25MB.toFixed(5), today)
+        await sendMessageToGraphite('ardrive.price.ar.100mb', +priceOf100MB.toFixed(5), today)
+        await sendMessageToGraphite('ardrive.price.ar.500mb', +priceOf500MB.toFixed(5), today)
+        await sendMessageToGraphite('ardrive.price.ar.1gb', +priceOf1GB.toFixed(5), today)
     
         // Get the data prices in USD
-        await sendMessageToGraphite('price.usd.1mb', (+priceOf1MB.toFixed(5) * arUSDPrice), today)
-        await sendMessageToGraphite('price.usd.5mb', (+priceOf5MB.toFixed(5) * arUSDPrice), today)
-        await sendMessageToGraphite('price.usd.25mb', (+priceOf25MB.toFixed(5) * arUSDPrice), today)
-        await sendMessageToGraphite('price.usd.100mb', (+priceOf100MB.toFixed(5) * arUSDPrice), today)
-        await sendMessageToGraphite('price.usd.500mb', (+priceOf500MB.toFixed(5) * arUSDPrice), today)
-        await sendMessageToGraphite('price.usd.1gb', (+priceOf1GB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.1mb', (+priceOf1MB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.5mb', (+priceOf5MB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.25mb', (+priceOf25MB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.100mb', (+priceOf100MB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.500mb', (+priceOf500MB.toFixed(5) * arUSDPrice), today)
+        await sendMessageToGraphite('ardrive.price.usd.1gb', (+priceOf1GB.toFixed(5) * arUSDPrice), today)
     }
 }
 
